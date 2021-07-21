7b405d31a432bca70499ed49d1a5a2660ff975c2
Justin Stoddard <justin.stoddard@go1.com>
2021-07-21 14:52:13 -0600

    added test for login button

--- src/Components/Nav/LoginButton/__tests__/LoginButton.test.tsx
+++ src/Components/Nav/LoginButton/__tests__/LoginButton.test.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+
 import { ThemeProvider } from '@material-ui/core';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import LoginButton from '../index';
 import theme from '../../../../Theme';
+import { act } from 'react-dom/test-utils';
+
+const mockHandleClick = jest.fn();
+
+jest.mock('@auth0/auth0-react', () => ({
+  Auth0Provider: ({ children }: any) => children,
+  withAuthenticationRequired: ((component: any, _: any) => component),
+  useAuth0: () => ({
+    isLoading: false,
+    user: { sub: "foobar" },
+    isAuthenticated: true,
+    loginWithRedirect: mockHandleClick
+  })
+}));
 
 describe('Login Button', () => {
 
   beforeEach(() => {
     render(
-      <ThemeProvider theme={theme}>
-        <LoginButton />
-      </ThemeProvider>
+      <Auth0Provider domain="xxxx" clientId="xxxx">
+        <ThemeProvider theme={theme}>
+          <LoginButton />
+        </ThemeProvider>
+      </Auth0Provider>
     );
   });
 
@@ -22,5 +40,16 @@ describe('Login Button', () => {
     const button = getByTestId("nav-login-button");
 
     expect(button).toBeInTheDocument();
+  });
+
+  it('Clicks when onClick prop is triggered', () => {
+    const { getByTestId } = screen;
+    const button = getByTestId("nav-login-button");
+
+    act(() => {
+      fireEvent.click(button);
+    });
+
+    expect(mockHandleClick).toHaveBeenCalledTimes(1);
   });
 });
